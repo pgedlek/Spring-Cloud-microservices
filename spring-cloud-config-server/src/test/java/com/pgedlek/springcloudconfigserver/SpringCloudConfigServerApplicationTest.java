cee0554f7b56609d46817e46dc3fe7dd6bb1eb4a
Pawel Gedlek <pawgedlek@gmail.com>
2025-01-18 23:23:41 +0100

    Implement spring cloud config server

---
+++ spring-cloud-config-server/src/test/java/com/pgedlek/springcloudconfigserver/SpringCloudConfigServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pgedlek.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
