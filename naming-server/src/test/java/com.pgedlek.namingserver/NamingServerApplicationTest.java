34ec2d7c7eb2e49a87c5ea8c00878ab523482e2f
Pawel Gedlek <pawgedlek@gmail.com>
2025-01-18 23:02:03 +0100

    Add Eureka discovery server as a spring module

---
+++ naming-server/src/test/java/com.pgedlek.namingserver/NamingServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pgedlek.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
